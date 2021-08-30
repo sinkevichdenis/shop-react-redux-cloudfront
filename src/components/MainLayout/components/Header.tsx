7858228c67bb9cec10c96785fdfda49dd255268c
Dzianis_Sinkevich1 <dzianis_sinkevich1@epam.com>
2021-08-30 03:37:19 +0300

    feat: add real product API

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
     <AppBar position="relative">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
-          <Link className={classes.homeLink} to="/">My Auto Store!</Link>
+          <Link className={classes.homeLink} to="/">The Best Pet Shop!</Link>
         </Typography>
 
         {auth && (
